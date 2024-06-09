4f49a39cc895bf134b2b4122d636cebc7d6717bd
Jeni <58479587+SleepyJeniCodes@users.noreply.github.com>
2024-06-09 12:20:33 -0400

    add initial setup for Express server and PostgreSQL db connection and add Japanese alphabet csv files for db setup

---
+++ index.js
@@ -0,0 +1,16 @@
+import express from "express";
+import bodyParser from "body-parser";
+import pg from "pg";
+
+const app = express();
+const port = 3000;
+
+const db = new pg.Client({
+    user: process.env.PG_USER,
+    host: process.env.PG_HOST,
+    database: process.env.PG_DATABASE,
+    password: process.env.PG_PASSWORD,
+    port: process.env.PG_PORT,
+});
+
+db.connect();
