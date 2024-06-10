0664748b7a1819a72c19317d4fd40a7697c7fe0d
Jeni <58479587+SleepyJeniCodes@users.noreply.github.com>
2024-06-10 09:42:56 -0400

    add the option of switching to katakana by using a dropdown menu. quiz is automatically set to hiragana. adjusted css to add dropdown menu

--- index.js
+++ index.js
@@ -18,15 +18,15 @@ const db = new pg.Client({
 db.connect();
 
 //Queries the database for quiz questions and populates the 'quiz' array
-let quiz = [];
-db.query("SELECT * FROM hiragana", (err, res) => {
-  if (err) {
-    console.error("Error executing query", err.stack);
-  } else {
-    quiz = res.rows; //stores the quiz questions retrieved from the database
-  }
-  db.end();
-});
+//let quiz = [];
+//db.query("SELECT * FROM hiragana", (err, res) => {
+  //if (err) {
+    //console.error("Error executing query", err.stack);
+  //} else {
+    //quiz = res.rows; //stores the quiz questions retrieved from the database
+  //}
+  //db.end();
+//});
 
 let totalCorrect = 0;
 
@@ -38,14 +38,19 @@ let currentQuestion = {};
 
 //GET home page
 app.get("/", async (req, res) => {
+    const charset = req.query.charset || 'hiragana' //default to hiragana if no charset is selected
     totalCorrect = 0; //resets the total correct count for a new session
-    await nextQuestion(); //fetches next question asynchronously
+    await nextQuestion(charset); //fetches next question asynchronously
     console.log(currentQuestion); //logs current question to the console
-    res.render("index.ejs", { question: currentQuestion }); //renders the index.ejs template
+    res.render("index.ejs", { 
+      question: currentQuestion,
+      selectedCharset: charset,
+      totalScore: totalCorrect
+     }); //renders the index.ejs template
   });
 
 // POST a new post
-app.post("/submit", (req, res) => {
+app.post("/submit", async (req, res) => {
     let answer = req.body.answer.trim(); //extracts and trims the submitted answer
     let isCorrect = false; //flag to track whether the answer is correct
     if (currentQuestion.romaji.toLowerCase() === answer.toLowerCase()) {
@@ -54,18 +59,22 @@ app.post("/submit", (req, res) => {
       isCorrect = true;
     }
 
-    nextQuestion(); //fetches the next question
+    const charset = req.body.charset || 'hiragana'; // Default to hiragana if no charset is selected
+    await nextQuestion(charset); // Fetches the next question based on selected charset
     res.render("index.ejs", {
         question: currentQuestion,
         wasCorrect: isCorrect, //passes whether the answer was correct to the ejs file
         totalScore: totalCorrect, //passes tht total correct count to the template
+        selectedCharset: charset // Passes the selected charset to the template
     });
 });
 
 //function to fetch the next question
-async function nextQuestion() {
-    const randomJapanese = quiz[Math.floor(Math.random() * quiz.length)]; //selects a random question from the 'quiz' array
-    currentQuestion = randomJapanese; //sets the current question to the randomly selected question
+async function nextQuestion(charset) {
+    const query = `SELECT * FROM ${charset}`;
+    const { rows } = await db.query(query);
+    const randomCharacter = rows[Math.floor(Math.random() * rows.length)]; //selects a random question from the 'quiz' array
+    currentQuestion = randomCharacter; //sets the current question to the randomly selected question
   }
 
 app.listen(port, () => {
